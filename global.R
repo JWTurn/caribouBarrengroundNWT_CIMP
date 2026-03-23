4f9ac6f10e467b72a1e876d07854269b0792f0e5
Julie Turner <julwturner@gmail.com>
2026-03-23 15:24:59 -0400

    Initial commit

---
+++ global.R
@@ -0,0 +1,57 @@
+repos <- c("https://predictiveecology.r-universe.dev", getOption("repos"))
+source("https://raw.githubusercontent.com/PredictiveEcology/pemisc/refs/heads/development/R/getOrUpdatePkg.R")
+getOrUpdatePkg(c("Require", "SpaDES.project"), c("1.0.1.9024", "1.0.1.9000")) # only install/update if required
+#Require::Install("PredictiveEcology/SpaDES.core@development")
+
+projPath = "~/git-local/caribouBarrengroundNWT_CIMP"
+reproducibleInputsPath = "~/git-local/reproducibleInputs"
+
+out <- SpaDES.project::setupProject(
+  Restart = TRUE,
+  useGit = 'JWTurn',
+  updateRprofile = TRUE,
+  #overwrite = TRUE,
+  paths = list(projectPath =  projPath
+               #"packagePath" = file.path("packages", Require:::versionMajorMinor())
+  ),
+  options = options(spades.allowInitDuringSimInit = TRUE,
+                    spades.allowSequentialCaching = TRUE,
+                    spades.moduleCodeChecks = FALSE,
+                    spades.recoveryMode = 1,
+                    reproducible.inputPaths = reproducibleInputsPath,
+                    reproducible.useMemoise = TRUE
+                    ,reproducible.cloudFolderID = 'https://drive.google.com/drive/folders/1lDVP0G1FFft5WJgnKBLPPlkTXPsU04hr?usp=share_link'
+  ),
+  modules = c('JWTurn/RSFpredict@main'
+              
+  ),
+  params = list(
+    .globals = list(
+      .plots = c("png"),
+      .studyAreaName=  "bathurst",
+      .useCache = c(".inputObjects")
+    )
+    
+    
+  ),
+  
+  packages = c('RCurl', 'XML', 'snow', 'googledrive', 'httr2', "terra", "gert", "remotes",
+               "PredictiveEcology/reproducible@development", "PredictiveEcology/LandR@development",
+               "PredictiveEcology/SpaDES.core@development"),
+  
+  model = reproducible::prepInputs(url = 'https://drive.google.com/file/d/1dZD0NbihMMyVsFiQAkqHbeXYFYDSAwwT/view?usp=share_link',
+                                       fun = 'readRDS',
+                                       destinationPath = 'inputs')
+  
+  # OUTPUTS TO SAVE -----------------------
+  # outputs = {
+  #   # save to disk 2 objects, every year
+  #   #will add once works, ha
+  #
+  # }
+  
+)
+
+
+results <- SpaDES.core::simInitAndSpades2(out)
+results <- SpaDES.core::restartSpades()

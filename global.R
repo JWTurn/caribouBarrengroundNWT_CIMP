738f77ca84ccefd8d7c2553d7e02b21a941d4dca
Julie Turner <julwturner@gmail.com>
2026-03-25 21:41:17 -0400

    update global

--- global.R
+++ global.R
@@ -5,6 +5,7 @@ getOrUpdatePkg(c("Require", "SpaDES.project"), c("1.0.1.9024", "1.0.1.9000")) # 
 
 projPath = "~/git-local/caribouBarrengroundNWT_CIMP"
 reproducibleInputsPath = "~/git-local/reproducibleInputs"
+lapply(dir('R', '*.R', full.names = TRUE), source)
 
 out <- SpaDES.project::setupProject(
   Restart = TRUE,
@@ -22,34 +23,46 @@ out <- SpaDES.project::setupProject(
                     reproducible.useMemoise = TRUE
                     ,reproducible.cloudFolderID = 'https://drive.google.com/drive/folders/1lDVP0G1FFft5WJgnKBLPPlkTXPsU04hr?usp=share_link'
   ),
-  modules = c('JWTurn/RSFpredict@main'
-              
+  modules = c(
+              'JWTurn/RSFpredict@main'
+
   ),
   params = list(
     .globals = list(
       .plots = c("png"),
       .studyAreaName=  "bathurst",
+      outputFolderID = 'https://drive.google.com/drive/folders/1-OzIDz6azh39fcGsk5llZaGrqwvR-rBl?usp=share_link',
       .useCache = c(".inputObjects")
     )
-    
-    
+
+
   ),
-  
-  packages = c('RCurl', 'XML', 'snow', 'googledrive', 'httr2', "terra", "gert", "remotes",
-               "PredictiveEcology/reproducible@development", "PredictiveEcology/LandR@development",
+
+  packages = c('RCurl', 'XML', 'snow', 'googledrive', 'httr2', "terra", "gert", "remotes", 'glmmTMB',
+               "PredictiveEcology/reproducible@development", #"PredictiveEcology/LandR@development",
                "PredictiveEcology/SpaDES.core@development"),
-  
-  model = reproducible::prepInputs(url = 'https://drive.google.com/file/d/1dZD0NbihMMyVsFiQAkqHbeXYFYDSAwwT/view?usp=share_link',
+
+  model = reproducible::prepInputs(url = 'https://drive.google.com/file/d/1HfMxbSrRUvpdL_R6OCHrVzh1Ce50rPrx/view?usp=share_link',
                                        fun = 'readRDS',
-                                       destinationPath = 'inputs')
-  
+                                       destinationPath = 'inputs'),
+
+  studyArea = reproducible::prepInputs(url = 'https://drive.google.com/file/d/1dIfh6_iK8mSmjhGJ56e0mz19q_fqgcAE/view?usp=share_link',
+                                       fun =  'terra::vect',
+                                       destinationPath = 'inputs',
+                                       targetFile = 'Bathurst_Calving_MCP_females_only_2005_19.shp'),
+
+  studyAreaLarge = terra::buffer(studyArea, 50000),
+
+  landStack = makeBarrengroundLand(studyAreaLarge, inPath = reproducibleInputsPath, dPath = 'inputs') |>
+    reproducible::Cache(.functionName = "landStack")
+
   # OUTPUTS TO SAVE -----------------------
   # outputs = {
   #   # save to disk 2 objects, every year
   #   #will add once works, ha
   #
   # }
-  
+
 )
 
 
